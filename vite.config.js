15526c1aa12d40b60438fadb0323e6bab1fb60d1
mizupe <82708314+fumifumi0831@users.noreply.github.com>
2025-03-12 07:44:51 +0900

    環境変数の設定を追加するためにvite.config.jsを更新

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			// セキュリティの都合上、プロジェクトルート以外のファイルへのアクセスを許可しない
+			strict: true
+		}
+	},
+	// 環境変数の設定
+	envPrefix: 'OPENAI_'
 });
